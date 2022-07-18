8b5638d9c2589e8efd80d755b16713a60ddcf63c
Zerdestr <ajiekc1004@gmail.com>
2022-07-18 18:38:33 +0300

    made a couple of sections and blocks

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,4 +2,31 @@ $(function () {
 
   $('.header__catalog').styler();
 
+  const discountSwiper = new Swiper('.discount-swiper', {
+
+
+    loop: true,
+
+    wrapperClass: 'discount-swiper__wrapper',
+
+    slideClass: 'discount-swiper__slide',
+
+
+    pagination: {
+      el: '.swipers-pagination',
+      type: 'bullets',
+      bulletClass: 'swipers__pagination-bullet',
+      bulletActiveClass: 'swipers__pagination-bullet--active',
+      clickable: true
+    },
+
+
+    navigation: {
+      nextEl: '.swipers-button-next',
+      prevEl: '.swipers-button-prev',
+    },
+
+
+  });
+
 });
